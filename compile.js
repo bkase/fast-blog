cebb91fadb867f233ecaa6c0949da4b1688b7916
Brandon Kase <bkase@cmu.edu>
2013-08-10 19:40:42 -0700

    Made homepage logic

--- compile.js
+++ compile.js
@@ -1,16 +1,27 @@
-const posts = ['test.md']
+const posts = ['test.md', 'test2.md']
 const marked = require("marked");
+const $ = require("cheerio");
 
 const dustfs = require("dustfs");
 const fs = require("fs");
 dustfs.dirs('templates');
 
-posts.forEach(function(post) {
-  mdData = fs.readFileSync(post);
-  console.log(mdData.toString());
-  dustfs.render('a-post.dust', { compiled: marked(mdData.toString()) }, function(err, out) {
-    console.log(out);
+var mostRecent = [];
+posts.reverse().forEach(function(post, i) {
+  var mdData = fs.readFileSync(post);
+  var html = marked(mdData.toString());
+  var title = $('h1', html);
+  var intro = title.next();
+  if (i < 7) {
+    mostRecent.push({ intro: title.toString() + intro.toString() });
+  }
+
+  dustfs.render('a-post.dust', { compiled: html }, function(err, out) {
     fs.writeFileSync(post.replace(/\.md/, ".html"), out);
   });
+});
+
+dustfs.render('homepage.dust', { intros: mostRecent }, function(err, out) {
+  fs.writeFileSync("homepage.html", out);
 });
 
---
+++ test2.md
@@ -0,0 +1,4 @@
+# A Newer Articles
+
+Head asd jlaj sdjajsd a sjdj asjd aks jdaks jdaks dakjs dakjs akjs akjs dkajs kajs dkajs dkajs dkajs dkajs dkajs dkajs dkajs dkajs dkajs dakjs dakjs d
+
